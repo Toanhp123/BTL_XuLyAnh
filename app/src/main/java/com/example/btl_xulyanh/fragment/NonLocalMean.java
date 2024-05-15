87275e5d051cde5ae4f3515b28bac235b1a2392f
Pham Tien Toan <toanpham12102003@gmail.com>
2024-05-15 22:17:31 +0700

    ver 1

--- app/src/main/java/com/example/btl_xulyanh/fragment/NonLocalMean.java
+++ app/src/main/java/com/example/btl_xulyanh/fragment/NonLocalMean.java
@@ -10,6 +10,7 @@ import android.widget.ImageView;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.btl_xulyanh.R;
@@ -36,7 +37,7 @@ public class NonLocalMean extends Fragment {
     });
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nlm, container, false);
 
         imageProcessor = new ImageProcessor();
