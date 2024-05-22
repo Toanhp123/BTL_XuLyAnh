2dbdb0104007b305cf2cc4b482d15fc5483aba01
Pham Tien Toan <toanpham12102003@gmail.com>
2024-05-22 10:11:42 +0700

    ver 1

--- app/src/main/java/com/example/btl_xulyanh/activitiy/MainApp.java
+++ app/src/main/java/com/example/btl_xulyanh/activitiy/MainApp.java
@@ -3,6 +3,7 @@ package com.example.btl_xulyanh.activitiy;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,6 +26,7 @@ import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 public class MainApp extends AppCompatActivity implements NonLocalMean.OnImageProcessedListener, Bilateral.OnImageProcessedListener {
@@ -74,11 +76,22 @@ public class MainApp extends AppCompatActivity implements NonLocalMean.OnImagePr
         } else if (item.getItemId() == R.id.itemAdd) {
             getImageLauncher.launch("image/*");
         } else if (item.getItemId() == R.id.itemSave) {
-            if (save_image != null) {
+            if (src_image != null && save_image != null) {
                 String time = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O ?
                         TimeTransfer.getTime(System.currentTimeMillis()) : String.valueOf(System.currentTimeMillis());
                 ImageProcessor.saveImage(this, save_image, "saved image at " + time);
                 Toast.makeText(this, "Lưu ảnh thành công", Toast.LENGTH_SHORT).show();
+            } else if (src_image != null) {
+                String time = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O ?
+                        TimeTransfer.getTime(System.currentTimeMillis()) : String.valueOf(System.currentTimeMillis());
+                Bitmap bitmap;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), src_image);
+                    ImageProcessor.saveImage(this, bitmap, "saved image at " + time);
+                    Toast.makeText(this, "Lưu ảnh thành công", Toast.LENGTH_SHORT).show();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             } else {
                 Toast.makeText(this, "Hãy chọn ảnh", Toast.LENGTH_SHORT).show();
             }
@@ -89,6 +102,7 @@ public class MainApp extends AppCompatActivity implements NonLocalMean.OnImagePr
     private final ActivityResultLauncher<String> getImageLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(), uri -> {
         if (uri != null) {
             imageView.setImageURI(uri);
+            src_image = uri;
             nonLocalMean.setUri(uri);
             bilateral.setUri(uri);
         }
